b0dc249df2c0cd23a1928f535bbeddc3ffd78cef
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-16 14:49:12 -0700

    Added functionality to form

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 
 var $form = document.querySelector('form');
+var $addEntry = document.querySelector('.add-entry');
+var $overLay = document.querySelector('.overlay');
 
 $form.addEventListener('submit', handleSubmit);
+$addEntry.addEventListener('click', handleClick);
+
 function handleSubmit(event) {
   event.preventDefault();
-  // var $elements = $form.elements;
-  // var $day = $elements.day;
+  var $elements = $form.elements;
+  var $day = $elements.day.value;
+  var $time = $elements.time.value;
+  var $description = $elements.description.value;
+  $overLay.className = 'overlay hidden';
+  // console.log('day', $day, 'time', $time, 'description', $description);
+  var entry = {
+    time: $time,
+    description: $description
+  };
+  saveEntry($day, entry);
+}
+
+function handleClick(event) {
+  $overLay.className = 'overlay';
+}
+
+function saveEntry(day, entry) {
+
 }
