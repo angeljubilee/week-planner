bae8d8e50a356db227fb469e6fd20dbf9198b106
CooperKile <Cooperk1807@gmail.com>
2021-07-16 13:29:18 -0700

    Worked on submit event.

---
+++ main.js
@@ -0,0 +1,9 @@
+
+var $form = document.querySelector('form');
+
+$form.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  // var $elements = $form.elements;
+  // var $day = $elements.day;
+}
